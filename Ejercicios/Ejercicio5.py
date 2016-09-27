3ceb334b09a4ac76101e67b9ce15be535300b8d2
IhToN <ihton93@gmail.com>
2016-09-27 10:15:38 +0200

    Ejercicio 6

--- Ejercicios/Ejercicio5.py
+++ Ejercicios/Ejercicio5.py
@@ -11,3 +11,15 @@ while True:  # Hacemos que el while esté SIEMPRE ejecutándose
         print('Okay, I\'ll stop making questions.')
         break  # Lanzamos el mágico break para que ROMPA el while
 print('\033[93m*** IA has stopped working.')  # La cosa esa del \033[93m es para darle colorcito amarillo al texto
+
+"""
+    - Modo estructurado -
+    x = 1
+    while x != 0:
+        x = float(input('Number? ')
+        if x > 0:
+            print('Positive')
+        if x < 0:
+            print('Negative')
+    print('EOP')
+"""
---
+++ Ejercicios/Ejercicio6.py
@@ -0,0 +1,14 @@
+"""
+    Escribe un programa que compruebe si la última letra de una cadena es una A que suelte un mensaje
+"""
+while True:
+    inputString = input('Would you please intruduce a phrase?\n')
+
+    if str(inputString[-1]).casefold() == 'a':  # Forzamos la conversión a str y le hacemos casefold (tó a minúsculas)
+        print('I like that phrase, you must be a nice guy.')
+        print('Introduced phrase:', inputString)
+        print('Length of the phrase:', len(inputString))
+    else:
+        print('I don\'t like you, neither your way of thinking.')
+        print('Introduced phrase:', inputString)
+        print('Length of the phrase:', len(inputString))
