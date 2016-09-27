ae9322cab336bbe783a8de3daaf1228cdc413489
IhToN <ihton93@gmail.com>
2016-09-27 09:24:07 +0200

    Ej5 - Use a break instead of a real condition in the while statement

--- Ejercicios/Ejercicio5.py
+++ Ejercicios/Ejercicio5.py
@@ -1,12 +1,11 @@
 """
     Escribe un programa que reconozca la entrada de un número y nos diga si es positivo o negativo. Que pregunte constantemente hasta que el usuario introduzca un Cero.
 """
-loopCondition = True
-while loopCondition:
+while True:
     x = float(input('Which number would you like to check? \n'))
     if x > 0:
         print(x, '- It\'s a positive number.')
     elif x < 0:
         print(x, '- It\'s negative.')
     else:
-        loopCondition = False
+        break
