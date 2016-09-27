dcf33776dd8e848841cdc264f2dad35ed2860658
IhToN <ihton93@gmail.com>
2016-09-27 08:58:47 +0200

    fixes

--- Ejercicios/Ejercicio4.py
+++ Ejercicios/Ejercicio4.py
@@ -1,7 +1,7 @@
 """
     Escribe un programa que reconozca una entrada de un número y nos diga si es negativo, positivo o cero.
 """
-x = int(input('Introduce un número: '))
+x = float(input('Introduce un número: '))       # Si usas int() en vez de float() sólo reconoce enteros y no decimales.
 if x > 0:       # La condición puede ir entre paréntesis (x > 0) aunque es desaconsejable (en Python)
     print(x, 'es positivo.')
 elif x == 0:
--- Teoria/InputTesting.py
+++ Teoria/InputTesting.py
@@ -2,3 +2,6 @@
 
 cadena_n = input('Introduce un número')
 # Convertimos la cadena a un entero
+x = int(cadena_n)
+print('Tipo del input: ', type(cadena_n))
+print('Tipo de la conversión: ', type(x))
