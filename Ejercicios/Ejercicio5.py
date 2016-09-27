885f0eca1ce5dc3b08c8e1d0f3ae1f7bb333ea88
IhToN <ihton93@gmail.com>
2016-09-27 09:28:26 +0200

    Ej5 - Formateado

--- Ejercicios/Ejercicio5.py
+++ Ejercicios/Ejercicio5.py
@@ -1,11 +1,11 @@
 """
     Escribe un programa que reconozca la entrada de un número y nos diga si es positivo o negativo. Que pregunte constantemente hasta que el usuario introduzca un Cero.
 """
-while True:     # Hacemos que el while esté SIEMPRE ejecutándose
+while True:  # Hacemos que el while esté SIEMPRE ejecutándose
     x = float(input('Which number would you like to check? \n'))
-    if x > 0:       # Miramos si es positivo
+    if x > 0:  # Miramos si es positivo
         print(x, '- It\'s a positive number.')
-    elif x < 0:     # Si no es positivo, miramos si es negativo
+    elif x < 0:  # Si no es positivo, miramos si es negativo
         print(x, '- It\'s negative.')
-    else:           # Si no es ni uno ni otro, es cero, por lo tanto acabamos el while
-        break       # Lanzamos el mágico break para que ROMPA el while
+    else:  # Si no es ni uno ni otro, es cero, por lo tanto acabamos el while
+        break  # Lanzamos el mágico break para que ROMPA el while
