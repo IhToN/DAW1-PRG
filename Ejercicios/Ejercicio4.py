7ecd2294c60b2789940bc2f7b5585bae3d828ace
IhToN <ihton93@gmail.com>
2016-09-27 09:11:38 +0200

    Moar fixes Ej4

--- Ejercicios/Ejercicio4.py
+++ Ejercicios/Ejercicio4.py
@@ -1,10 +1,10 @@
 """
     Escribe un programa que reconozca una entrada de un número y nos diga si es negativo, positivo o cero.
 """
-x = float(input('Introduce un número: '))  # Si usas int() en vez de float() sólo reconoce enteros y no decimales.
+x = float(input('Which number would you like to check? \n'))  # Si usas int() en vez de float() sólo reconoce enteros y no decimales.
 if x > 0:  # La condición puede ir entre paréntesis (x > 0) aunque es desaconsejable (en Python)
-    print(x, 'es positivo.')
+    print(x, '- It\'s a positive number.')
 elif x == 0:
-    print(x, 'es cero.')
+    print(x, '- It\'s zero.')
 else:
-    print(x, 'es negativo.')
+    print(x, '- It\'s negative.')
