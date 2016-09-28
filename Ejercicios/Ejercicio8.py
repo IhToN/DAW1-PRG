3d74f3219dd1490c7c7c15c28bd032fb42a4638c
IhToN <ihton93@gmail.com>
2016-09-28 09:41:33 +0200

    Ejercicio 8

---
+++ Ejercicios/Ejercicio8.py
@@ -0,0 +1,28 @@
+"""
+    Mostrar una cadena por líneas mostrando en la primera línea el primer carácter,
+    en la segunda línea el primer y segundo carácter, y así hasta la cadena final donde muestra la cadena completa.
+"""
+# Declaramos las variables principales que vamos a usar, aquí si debemos repetir el loop y los mensajes estáticos
+repeatLoop = True
+askMsg = 'Would you please intruduce a phrase?\n'
+bypassMsg = 'no me da la gana'
+stopMsg = 'Ok, don\'t hit me, I won\'t ask you anymore.'
+
+# Pedimos la String, comprobamos si es el bypass message y si no inicializamos contador
+inputString = input(askMsg)
+if str(inputString).casefold() == bypassMsg:
+    print(stopMsg)
+    repeatLoop = False
+i = 0
+
+# Entramos en el loop, procesamos los caracteres y luego preguntamos de nuevo al usuario por otra frase
+while repeatLoop:
+    while i <= len(inputString):
+        print(inputString[0:i])  # Usamos Slicing
+        i += 1
+    print('The String has', len(inputString), 'elements.')
+
+    inputString = input(askMsg)
+    if str(inputString).casefold() == bypassMsg:
+        print(stopMsg)
+        repeatLoop = False
