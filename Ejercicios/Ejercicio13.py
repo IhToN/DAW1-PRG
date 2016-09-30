7db48e6fc44547dfdb4abca2cffefe2c3b450b97
IhToN <ihton93@gmail.com>
2016-09-30 09:05:15 +0200

    Ejercicio 13

--- Ejercicios/Ejercicio13.py
+++ Ejercicios/Ejercicio13.py
@@ -7,6 +7,40 @@
 # Inicializamos las variables
 promptMsg = 'Introduce a number:\n'
 repeat = True
+totalTwo = 0
+totalThree = 0
+totalBoth = 0
+totalSum = 0
+totalCount = 0
+
+# Usamos el loop para preguntar cuantos números queremos, usamos float para admitir decimales
+while repeat:
+    inNum = float(input(promptMsg))
+    if inNum == 0:
+        repeat = False
+    else:
+        if inNum % 2 == 0:  # Comprobamos los múltiplos de dos
+            totalTwo += 1
+        if inNum % 3 == 0:  # Comprobamos los múltiplos de tres
+            totalThree += 1
+        if inNum % 2 == 0 and inNum % 3 == 0:  # Comprobamos los múltiplos de los dos
+            totalBoth += 1
+        totalSum += inNum  # Sumamos el número al total
+        totalCount += 1  # Aumentamos en un el total de cuentas
+
+print('Total numbers: \n', totalCount)
+print('Total sum: \n', totalSum)
+print('Multiples of Two: \n', int(totalTwo))
+print('Multiples of Three: \n', int(totalThree))
+print('Multiples of Two & Three: \n', int(totalBoth))
+
+"""
+    Solución usando Listas, así mostramos los números en sí y no las cantidades
+
+
+# Inicializamos las variables
+promptMsg = 'Introduce a number:\n'
+repeat = True
 numList = []
 
 # Usamos el loop para preguntar cuantos números queremos, usamos float para admitir decimales
@@ -18,7 +52,7 @@ while repeat:
         numList.append(inNum)
 
 # Inicializamos las listas a devolver
-parNum = []
+twoNum = []
 threeNum = []
 bothNum = []
 sumNum = 0
@@ -26,7 +60,7 @@ sumNum = 0
 # Tratamos cada elemento de la lista total de números
 for num in numList:
     if num % 2 == 0:  # Comprobamos los múltiplos de dos
-        parNum.append(num)
+        twoNum.append(num)
     if num % 3 == 0:  # Comprobamos los múltiplos de tres
         threeNum.append(num)
     if num % 2 == 0 and num % 3 == 0:  # Comprobamos los múltiplos de los dos
@@ -36,6 +70,7 @@ for num in numList:
 # Imprimimos los números pedidos
 print('Total numbers: \n', len(numList))
 print('Total sum: \n', sumNum)
-print('Multiples of Two: \n', parNum)
+print('Multiples of Two: \n', twoNum)
 print('Multiples of Three: \n', threeNum)
 print('Multiples of Two & Three: \n', bothNum)
+"""
