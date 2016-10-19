4ed0596b677531dc1b1f77b2bac6f874264d78d1
IhToN <ihton93@gmail.com>
2016-10-19 09:20:05 +0200

    Ej28

---
+++ Ejercicios/Ejercicio28.py
@@ -0,0 +1,18 @@
+"""
+    Queremos construir una lista por comprensión que contenga, de un texto dado,
+    guarde los caracteres de la cadena según su posición
+    'Hola Pepe' => ['H', 'Ho', 'Hol', …, 'Hola Pepe']
+"""
+
+
+def concat_text(cadena):
+    """
+    Devuelve una lista que contiene una cadena por concatenación de caracteres:
+    'Hola Pepe' => ['H', 'Ho', 'Hol', …, 'Hola Pepe']
+    :param cadena:
+    :return:
+    """
+    return [cadena[0:i] for i in range(1, len(cadena))]
+
+
+print(concat_text('hijo de puta, hay que decirlo más... hijo de puta más...'))
