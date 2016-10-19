fd3c1984c91386dc5ee3b995c477f705b5354e31
IhToN <ihton93@gmail.com>
2016-10-19 10:14:00 +0200

    Ej28 c

--- Ejercicios/Ejercicio28.py
+++ Ejercicios/Ejercicio28.py
@@ -1,17 +1,18 @@
 """
-    Queremos construir una lista por comprensión que contenga, de un texto dado, guarde los caracteres de la cadena según su posición
+    Queremos construir una lista por comprensión que contenga, de un texto dado, guarde los caracteres
+    de la cadena según su posición
     a. Primera solución
         'Hola Pepe' => ['H', 'Ho', 'Hol', …, 'Hola Pepe']
     b. Invertir las cadenas
             'Hola Pepe' => ['H', 'oH', 'loH', …, 'epeP olaH']
+    c. Construir una lista que contenga el tamaño de cada elemento de la lista de la solución a
 """
 
 
 def concat_text(cadena):
     """
     Devuelve una lista que contiene una cadena por concatenación de caracteres:
-    sol a = 'Hola Pepe' => ['H', 'Ho', 'Hol', …, 'Hola Pepe']
-    sol b = sol a = 'Hola Pepe' => ['Hola Pepe', 'Hola Pep', 'Hola Pe', …, 'H']
+    'Hola Pepe' => ['H', 'Ho', 'Hol', …, 'Hola Pepe']
     :param cadena:
     :return:
     """
@@ -19,10 +20,29 @@ def concat_text(cadena):
 
 
 def invert_concat_text(cadena):
-    return [x[::-1] for x in concat_text(cadena)]
+    """
+    Devuelve una lista que contiene una cadena por concatenación de caracteres invertida:
+    'Hola Pepe' => ['H', 'oH', 'loH', …, 'epeP olaH']
+    :param cadena:
+    :return:
+    """
+    return [elem[::-1] for elem in concat_text(cadena)]
+
+
+def len_concat_text(cadena):
+    """
+    Devuelve la longitud de cada elemento de concat_text(cadena)
+    :param cadena:
+    :return:
+    """
+    return [len(elem) for elem in concat_text(cadena)]
 
 
 test_cadena = 'hijo de puta, hay que decirlo más... hijo de puta más...'
 
+print('-------------- Solución A --------------')
 print(concat_text(test_cadena))
+print('-------------- Solución B --------------')
 print(invert_concat_text(test_cadena))
+print('-------------- Solución C --------------')
+print(len_concat_text(test_cadena))
