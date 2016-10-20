0f010073f8c5a6e461657cdab3472d71bd7602be
IhToN <ihton93@gmail.com>
2016-10-20 08:40:54 +0200

    Ej29 b

--- Ejercicios/Ejercicio29.py
+++ Ejercicios/Ejercicio29.py
@@ -1,6 +1,10 @@
 """
     Construir una lista por comprensión que contenga todos los múltiplos de tres hasta una cifra estipulada.
+    b. Solución original
+    a. Extender a incluir los números múltiplos de 5 y 7
 """
+
+sol = 'b'
 
 
 def multiplos_tres(numero):
@@ -9,8 +13,12 @@ def multiplos_tres(numero):
     :param numero:
     :return:
     """
-    #devolver [x para x en el rango de x+1, incluirlo si x%3 = 0
-    return [x for x in range(numero + 1) if x % 3 == 0]
+    if sol == 'a':
+        # devolver [x para x en el rango de x+1, incluirlo si x%3 = 0
+        return [x for x in range(numero + 1) if x % 3 == 0]
+    elif sol == 'b':
+        # devolver [x para x en el rango de x+1, incluirlo si x%3, x%5 o x%7 es 0
+        return [x for x in range(numero + 1) if x % 3 == 0 or x % 5 == 0 or x % 7 == 0]
 
 
 print(multiplos_tres(1000))
