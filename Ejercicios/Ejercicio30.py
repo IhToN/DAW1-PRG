40a00c707838ea80152b42d05e4df749e696440b
IhToN <ihton93@gmail.com>
2016-10-20 09:07:02 +0200

    Ej30

--- Ejercicios/Ejercicio30.py
+++ Ejercicios/Ejercicio30.py
@@ -1,6 +1,8 @@
 """
     Crear una lista por comprensión que contenga todas las sílabas posibles en castellano
     cuya primera letra sea una consonante y cuya segunda sea una vocal.
+    a. Solucion original
+    b.
 """
 
 
@@ -8,4 +10,9 @@ def silabas_convoc():
     return [con + voc for con in 'bcdfghjklmnñpqrstvwxyz' for voc in 'aeiou']
 
 
-print(silabas_convoc())
+def palabras_ds():
+    return [sil1 + sil2 for sil1 in silabas_convoc() for sil2 in silabas_convoc()]
+
+
+print(len(silabas_convoc()), silabas_convoc())
+print(len(palabras_ds()), palabras_ds())
