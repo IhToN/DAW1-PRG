e4af27b1343c57dc5d272b189639b5cae5f625ba
IhToN <ihton93@gmail.com>
2016-10-25 09:19:56 +0200

    Ej33

--- Ejercicios/Ejercicio33.py
+++ Ejercicios/Ejercicio33.py
@@ -18,6 +18,12 @@ x1 = int(input('Introduce el primer punto (x):\n'))
 y1 = int(input('Introduce el primer punto (y):\n'))
 punto1 = x1, y1
 # El segundo punto, como somos prós, pedimos que el usuario ponga x,y
-punto2 = tuple(int(x) for x in input('Introduce el segundo punto (y):\n').split(","))
+''' Traduciendo la frase de abajo es:
+ punto2 va a ser una tupla, la tupla va a ser igual que convertir a integer x.
+ la x es cada elemento que hay en dividir la cadena introducida según sus comas
+ es decir:
+ 1,4,5,6 la x irá pasando siendo x = 1, x = 4, x = 5, x = 6
+'''
+punto2 = tuple(int(x) for x in input('Introduce el segundo punto (x,y):\n').split(","))
 
 print('Suma vectorial de', punto1, 'y', punto2, '=', suma_vectorial(punto1, punto2))
