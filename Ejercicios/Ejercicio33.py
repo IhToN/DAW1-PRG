58477363b32569e9ecf80409a301836e372a608f
IhToN <ihton93@gmail.com>
2016-10-25 09:18:20 +0200

    Ej33

--- Ejercicios/Ejercicio33.py
+++ Ejercicios/Ejercicio33.py
@@ -13,12 +13,11 @@ def suma_vectorial(p1, p2):
     return p1[0] + p2[0], p1[1] + p2[1]  # Sumamos ambas X y sumamos ambas Y, devolviendo una tupla
 
 
-# Preguntamos e inicializamos los puntos
+# Preguntamos e inicializamos los puntos, el primer punto preguntamos valor a valor
 x1 = int(input('Introduce el primer punto (x):\n'))
 y1 = int(input('Introduce el primer punto (y):\n'))
 punto1 = x1, y1
-x2 = int(input('Introduce el segundo punto (x):\n'))
-y2 = int(input('Introduce el segundo punto (y):\n'))
-punto2 = x2, y2
+# El segundo punto, como somos prós, pedimos que el usuario ponga x,y
+punto2 = tuple(int(x) for x in input('Introduce el segundo punto (y):\n').split(","))
 
 print('Suma vectorial de', punto1, 'y', punto2, '=', suma_vectorial(punto1, punto2))
