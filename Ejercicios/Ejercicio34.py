78c7c9e41be5974fec73c722fec17748433799c8
IhToN <ihton93@gmail.com>
2016-10-27 10:18:47 +0200

    Ej34d

--- Ejercicios/Ejercicio34.py
+++ Ejercicios/Ejercicio34.py
@@ -5,6 +5,7 @@
     b. Hacer otra función resumen(notas) a la que dada la tupla nos devuelva otra tupla
     que conste de tres elementos internos: cantidad de elementos, la suma de ellos y la media aritmética
     c. Generar entre 10 y 20 notas al azar para 40 alumnos distintos.
+    d. Definir una función que imprima el resumen de las notas de los alumnos.
 """
 from random import randint
 
@@ -30,17 +31,31 @@ def resumen(notas):
 
 
 def gen_notas():
+    """ Devuelve una tupla de notas aleatorias entre 0 y 10, con una cantidad de entre 10 y 20 elementos
+    """
     ret = ()
     for i in range(randint(10, 20)):
         ret += (randint(0, 10),)
     return ret
 
 
-def gen_alumnos():
+def gen_alumnos(cant):
+    """ Devuelve una tupla con notas aleatorias para cant alumnos
+    """
     ret = ()
-    for i in range(40):
+    for i in range(cant):
         ret += (gen_notas(),)
     return ret
+
+
+def imprimir_notas():
+    """ Imprime las notas aleatorias de 40 alumnos
+    """
+    id = 1
+    for i in gen_alumnos(40):
+        print('Notas del alumno ', id)
+        print(i, resumen(i))
+        id += 1
 
 
 test = 1, 2, 3, 4, 5, 6, 7
@@ -49,8 +64,4 @@ print('Total notas:', resumen(test)[1])
 print('Media notas:', resumen(test)[2])
 
 print('-----')
-id = 1
-for i in gen_alumnos():
-    print('Notas del alumno ', id)
-    print(i, resumen(i))
-    id += 1
+print(imprimir_notas())
