2d557fd478579b33a1d7ca784e400b9011fa6b79
IhToN <ihton93@gmail.com>
2016-10-27 08:35:57 +0200

    Ej34

--- Ejercicios/Ejercicio34.py
+++ Ejercicios/Ejercicio34.py
@@ -1,5 +1,6 @@
 """
-    Escribir una función a la que se le da como parámetro una tupla de números enteros y devuelve la suma de todos los elementos.
+    Escribir una función a la que se le da como parámetro una tupla de números enteros
+    y devuelva la suma de todos los elementos de dicha tupla.
     a. Solución Original
     b.
 """
@@ -12,3 +13,5 @@ def suma_numeros(lista_numeros):
     for elem in lista_numeros:
         ret += elem
     return ret
+    # Otra posible solución es con la función sum(iterable)
+    # return sum(lista_numeros)
