11ab451681c0d7812540763f6e657a2f69e37c24
IhToN <ihton93@gmail.com>
2016-11-09 10:24:53 +0100

    Ej40

---
+++ Ejercicios/EJercicio40.py
@@ -0,0 +1,23 @@
+"""
+    Buscar todos los números impares NO primos menores que mil y contarlos. Y cuántos números pares y primos hay.
+"""
+from math import ceil, sqrt
+
+
+def es_primo(numero):
+    """ Comprueba si el numero es primo o no, devuelve un boolean
+    """
+    loop = 2
+    if numero < 2:
+        return False
+    while loop < ceil(sqrt(numero + 1)):
+        if numero % loop == 0:
+            return False
+        loop += 1
+    return True
+
+
+ninp = {x for x in range(1, 1000, 2) if not es_primo(x)}
+npp = {x for x in range(0, 1000, 2) if es_primo(x)} # El único número Par Primo es 2.
+
+print(len(ninp), len(npp))
