c915cc5f2b49c350f4c31696a8201cf333e52e06
IhToN <ihton93@gmail.com>
2016-11-09 10:06:41 +0100

    Ej38 y 39

--- Ejercicios/Ejercicio38.py
+++ Ejercicios/Ejercicio38.py
@@ -2,6 +2,7 @@
     Comprobar las Leyes de Demorgan
         a. not (AUB) = not A or not B
         b. not(AorB) = not A and not B
+    La negación de una conjunción es la desconjunción de la negación de los términos.
 """
 A, B = {1, 2, 3}, {2, 3, 4}
 Universal = {x for x in range(100)}
