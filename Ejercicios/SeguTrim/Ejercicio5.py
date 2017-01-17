8daf7ef90222609a70e41283eb50f8f82fb96e75
Antonio Terrero <atalgaba@gmail.com>
2017-01-17 10:18:19 +0100

    Ejercicio5

--- Ejercicios/SeguTrim/Ejercicio5.py
+++ Ejercicios/SeguTrim/Ejercicio5.py
@@ -5,9 +5,9 @@
     c. Colocar una pelota que rebote, añadiendo un ángulo de corrección. Se colocarán dos porterías y
         cuando la pelota entra por alguno de ellos se aumenta el valor de un contador.
 """
-import threading
+import multiprocessing.dummy as multiprocessing
 import turtle
-import random
+from random import randint
 
 screen = turtle.Screen()
 car1 = turtle.Turtle()
@@ -17,7 +17,9 @@ car2.shape('square')
 ball = turtle.Turtle()
 ball.shape('circle')
 
-stop_car = threading.Event()
+car1_size, car2_size, ball_size = [0, 0], [0, 0], [0, 0]
+
+stop_car = multiprocessing.Event()
 
 
 def initialize_game():
@@ -26,9 +28,9 @@ def initialize_game():
     car2.fillcolor('blue')
     ball.fillcolor('green')
 
-    car1.setheading(0)
-    car2.setheading(180)
-    ball.setheading(90)
+    # car1.setheading(0)
+    car2.left(180)
+    ball.left(170)
 
     car1.up()
     car2.up()
@@ -37,39 +39,76 @@ def initialize_game():
     car1.setpos(-400, 0)
     car2.setpos(400, 0)
 
-    move_car1 = threading.Thread(target=move_object, args=[car1, 90, stop_car])
-    move_car2 = threading.Thread(target=move_object, args=[car2, 90, stop_car])
-    move_ball = threading.Thread(target=move_object, args=[ball, -10])
+    car1_size[0], car1_size[1] = 20, 20
+    car2_size[0], car2_size[1] = 20, 20
+    ball_size[0], ball_size[1] = 20, 20
+
+    multiprocessing.Queue()
+    move_car1 = multiprocessing.Process(target=move_object, args=(car1, 90, stop_car,))
+    move_car2 = multiprocessing.Process(target=move_object, args=(car2, 90, stop_car,))
+    move_ball = multiprocessing.Process(target=move_object, args=(ball, -10,))
 
     move_car1.start()
     move_car2.start()
     move_ball.start()
 
 
-def move_object(object, variance=0, stop=threading.Event()):
-    """Movemos el objeto en pasos de 5"""
-    while not stop.is_set():
-        object.forward(5)
-        if not check_canvas(object):
-            object.right(90 + random.randint(variance))
-        if object == car1 or object == car2:
-            check_collision(car1, car2)
-
-
-def check_canvas(object):
+def check_canvas(turtobj):
     """Comprobamos si el objeto está dentro del canvas"""
-    width = screen.window_width() // 2 - (object.turtlesize()[0] * 2)
-    height = screen.window_height() // 2 - (object.turtlesize()[1] * 2)
-    xcor = object.xcor()
-    ycor = object.ycor()
+    if turtobj == car1:
+        turtobj_size = car1_size
+    elif turtobj == car2:
+        turtobj_size = car2_size
+    elif turtobj == ball:
+        turtobj_size = ball_size
+
+    width = screen.window_width() // 2 - turtobj_size[0]
+    height = screen.window_height() // 2 - turtobj_size[1]
+    xcor = turtobj.xcor()
+    ycor = turtobj.ycor()
     return not (width < xcor or -width > xcor or height < ycor or -height > ycor)
 
 
 def check_collision(obj1, obj2):
     """Comprobamos si los objetos han colisionado entre sí"""
-    if obj1.distance(obj2.pos()) < 30:
+    if obj1.distance(obj2) < car1_size[0]:
         print("Puuuum, y explotó")
         stop_car.set()
+
+
+def bounce(turtobj, horizontal=True):
+    if turtobj.xcor() > 0 and turtobj.ycor() > 0:
+        if horizontal:
+            ret = turtobj.towards(0, screen.window_height() // 2)
+    elif turtobj.xcor() > 0 and turtobj.ycor() < 0:
+        if horizontal:
+            ret = turtobj.towards(0, -screen.window_height() // 2)
+    elif turtobj.xcor() < 0 and turtobj.ycor() > 0:
+        if horizontal:
+            ret = turtobj.towards(0, -screen.window_height() // 2)
+    elif turtobj.xcor() > 0 and turtobj.ycor() > 0:
+        if horizontal:
+            ret = turtobj.towards(0, screen.window_height() // 2)
+    else:
+        ret = 90
+    return ret
+
+
+def move_object(turtobj, variance=0, stop=multiprocessing.Event()):
+    """Movemos el objeto en pasos de 5"""
+    while not stop.is_set():
+        turtobj.forward(5)
+        if not check_canvas(turtobj):
+            randvariance = randint(0, abs(variance))
+            if variance < 0:
+                randvariance = -randvariance
+            angle = turtobj.towards(0, turtobj.ycor())
+            if angle == 0:
+                angle = 180
+            turtobj.setheading(bounce(turtobj))
+            print(bounce(turtobj))
+        if turtobj == car1 or turtobj == car2:
+            check_collision(car1, car2)
 
 
 initialize_game()
