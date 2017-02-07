926327be35d76a6cda4b0058bcd9fcedfb8d66b4
Antonio Terrero <atalgaba@gmail.com>
2017-02-07 09:20:17 +0100

    Ej13

---
+++ Ejercicios/SeguTrim/Ejercicio13.py
@@ -0,0 +1,11 @@
+"""
+    Tablas de multiplicar bien formateadas.
+"""
+
+
+def tabla_del(numero, cuantos):
+    for i in range(cuantos+1):
+        print('{0:3d}  x {1:3d} = {2:3d}'.format(numero, i, numero * i))
+
+
+tabla_del(15, 25)
