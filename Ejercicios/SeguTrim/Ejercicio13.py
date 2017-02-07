addd1b0c4783846bf5d36d4a485bafc3aa3b97de
Antonio Terrero <atalgaba@gmail.com>
2017-02-07 09:46:34 +0100

    Ej13

--- Ejercicios/SeguTrim/Ejercicio13.py
+++ Ejercicios/SeguTrim/Ejercicio13.py
@@ -5,8 +5,8 @@
 
 def tabla_del(numero, cuantos):
     for i in range(cuantos + 1):
-        # print('{0:3d}  x {1:3d} = {2:3d}'.format(numero, i, numero * i))
-        print(str(numero).ljust(4), 'x'.center(3), str(i).rjust(4), '='.center(3), str(numero * i).rjust(4))
+        print('{0:<3d} {1:^3} {2:>3d} {3:^3} {4:>3d}'.format(numero, 'x', i, '=', numero * i))
+        # print(str(numero).ljust(4), 'x'.center(3), str(i).rjust(4), '='.center(3), str(numero * i).rjust(4))
 
 
 tabla_del(15, 25)
