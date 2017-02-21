64d61656ef40601b4965a05ff70e95dced805246
Antonio Terrero <atalgaba@gmail.com>
2017-02-21 09:09:56 +0100

    punto + traza

--- Ejercicios/SeguTrim/Objetos/Punto.py
+++ Ejercicios/SeguTrim/Objetos/Punto.py
@@ -1,7 +1,14 @@
 """
-    Calse Punto
+    Clase Punto
         coord x
         coord y
+        suma(punto)
+        resta(punto)
+
+    Clase Traza
+        instancias Punto en una Lista
+        añadir punto
+        comparar dos trazas (dos trazas serán iguales si sus puntos son iguales)
 """
 
 
@@ -13,6 +20,9 @@ class Punto:
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
     def suma(self, punto):
         """ Devuelve la suma vectorial del punto con otro.
         """
@@ -22,3 +32,26 @@ class Punto:
         """ Devuelve la resta vectorial del punto con otro.
         """
         return self.suma(-punto)
+
+
+class Traza:
+    def __init__(self, *args):
+        self.trazado = []
+        for arg in args:
+            if isinstance(arg, Punto):
+                self.trazado.append(arg)
+
+    def __str__(self):
+        out = ""
+        for punto in self.trazado:
+            out += str(punto)
+        return out
+
+    def __eq__(self, other):
+        return self.trazado == other.trazado
+
+    def add_punto(self, punto):
+        """ Añade un punto nuevo a la Traza
+        """
+        if isinstance(punto, Punto):
+            self.trazado.append(punto)
