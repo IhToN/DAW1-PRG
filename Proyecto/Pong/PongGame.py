bfeceefe22d32f38dfadc880bfe50c9df0e8caba
Antonio Terrero <atalgaba@gmail.com>
2017-03-07 12:20:25 +0100

    pong de los cojones

--- Proyecto/Pong/PongGame.py
+++ Proyecto/Pong/PongGame.py
@@ -1,239 +1,244 @@
-"""
-    Hacer un jueguecillo de un coche.
-    a. Buscar en la documentación una forma de ver el tamaño del canvas.
-    b. Comprobar una colisión entre dos coches, en caso de colisión cambiarles el color.
-    c. Colocar una pelota que rebote, añadiendo un ángulo de corrección. Se colocarán dos porterías y
-        cuando la pelota entra por alguno de ellos se aumenta el valor de un contador.
-"""
-from multiprocessing import dummy as multiprocessing
-import turtle
+from turtle import Turtle, Screen
 import random
 
-enable_ia1, enable_ia2 = True, True
-initial_delay = 3
 
-screen = turtle.Screen()
-screen.bgcolor('#0f0f0f')
+class Pelota(Turtle):
+    def __init__(self, partida, velocidad=3):
+        Turtle.__init__(self)
+        self.shape('circle')
+        self.fillcolor('green')
+        self.pencolor('black')
+        self.up()
+        self.partida = partida
+        self.velocidad = velocidad
+        self.moviendose = True
 
-screen.delay(initial_delay)
-car1 = turtle.Turtle()
-car1.shape('square')
-car1.shapesize(1, 5, 2)
-car2 = turtle.Turtle()
-car2.shape('square')
-car2.shapesize(1, 5, 2)
-ball = turtle.Turtle()
-ball.shape('circle')
-marker = turtle.Turtle()
+    def mover_pelota(self):
+        if self.moviendose:
+            self.forward(self.velocidad)
+            self.check_canvas()
+            self.check_bate()
+            if self.partida.jugador1.ia:
+                self.partida.jugador1.sety(self.ycor())
+            if self.partida.jugador2.ia:
+                self.partida.jugador2.sety(self.ycor())
 
-score = [0, 0]
+    def check_canvas(self):
+        """Comprobamos si el objeto está dentro del canvas"""
+        partida.screen.tracer(0)
+        if abs(self.xcor()) >= self.partida.screen.window_width() / 2 - 20:
+            if self.xcor() < 0:
+                self.partida.marcador.sumarpunto(1)
+                posneg = 1
+            if self.xcor() > 0:
+                partida.marcador.sumarpunto(0)
+                posneg = -1
+            self.home()
+            self.right(posneg * random.randint(155, 205))
+            self.partida.screen.delay(self.partida.initial_delay)
+            self.partida.marcador.refrescar()
+        if abs(self.ycor()) >= self.partida.screen.window_height() / 2 - 40:
+            self.right(self.heading() * 2)
+            self.fd(15)
+        partida.screen.tracer(1)
 
-stop_ball = multiprocessing.Event()
-stop_car1 = multiprocessing.Event()
-stop_car2 = multiprocessing.Event()
-
-
-def initialize_game():
-    """Inicializa los objetos principales, orientándolos y posicionándolos"""
-    draw_field()
-
-    marker.hideturtle()
-
-    car1.up()
-    car2.up()
-    ball.up()
-    marker.up()
-
-    car1.fillcolor('red')
-    car2.fillcolor('blue')
-    ball.fillcolor('green')
-    car1.pencolor('#000000')
-    car2.pencolor('#000000')
-    ball.pencolor('#000000')
-    marker.color('white')
-
-    car1.left(90)
-    car2.left(90)
-    ball.right(random.choice([-1, 1]) * random.randint(155, 205))
-
-    car1.setpos(-400, 0)
-    car2.setpos(400, 0)
-    marker.sety(screen.window_height() / 2 - 160)
-
-    draw_marker()
-    multiprocessing.Queue()
-    move_ball = multiprocessing.Process(target=move_turt_ball, args=(ball, 2, stop_ball))
-
-    move_ball.start()
-
-
-def check_canvas_ball(turtobj):
-    """Comprobamos si el objeto está dentro del canvas"""
-    screen.tracer(0)
-    if abs(turtobj.xcor()) >= screen.window_width() / 2 - 20:
-        if turtobj.xcor() < 0:
-            score[1] += 1
-            ball.right(random.randint(155, 205))
-        if turtobj.xcor() > 0:
-            score[0] += 1
-            ball.right(-random.randint(155, 205))
-        turtobj.home()
-        screen.delay(initial_delay)
-        draw_marker()
-    if abs(turtobj.ycor()) >= screen.window_height() / 2 - 40:
-        turtobj.right(turtobj.heading() * 2)
-        turtobj.fd(15)
-    screen.tracer(1)
-
-
-def check_canvas_car(turtobj, down=True):
-    """Comprobamos si el objeto está dentro del canvas"""
-    if down:
-        return turtobj.ycor() <= -screen.window_height() / 2 + 100
-    else:
-        return turtobj.ycor() >= screen.window_height() / 2 - 100
-
-
-def bounce_car1():
-    """Comprobamos si la pelota toca el coche 1"""
-    if int(car1.xcor()) - 20 <= int(ball.xcor()) <= int(car1.xcor()) \
-            and (int(car1.ycor()) - 50 <= int(ball.ycor()) + 10 and int(ball.ycor()) - 10 <= int(car1.ycor()) + 50):
-        car1.setx(car1.xcor() - 5)
-        screen.tracer(0)
-        if screen.delay() > 0:
-            if enable_ia1:
-                screen.delay(screen.delay() - 1)
-            else:
-                screen.delay(screen.delay() - .5)
-        angle = ball.distance(car1.xcor(), car1.ycor()) + random.randint(1, 25)
-        if ball.ycor() <= car1.ycor():
-            angle = -angle
-        ball.left(180 + angle)
-        screen.tracer(1)
-        ball.fd(15)
-        car1.setx(car1.xcor() + 5)
+    def check_bate(self):
+        """Comprobamos si la pelota toca algún jugador"""
+        if self.xcor() < 0:  # Choque con el jugador 1
+            if int(self.partida.jugador1.xcor()) - 20 <= int(self.xcor()) <= int(self.partida.jugador1.xcor()) \
+                    and (int(self.partida.jugador1.ycor()) - 50 <= int(self.ycor()) - 10 and int(
+                        self.ycor()) + 10 <= int(self.partida.jugador1.ycor()) + 50):
+                self.partida.jugador1.setx(self.partida.jugador1.xcor() - 1)
+                self.partida.jugador1.setx(self.partida.jugador1.xcor() - 2)
+                self.partida.jugador1.setx(self.partida.jugador1.xcor() - 3)
+                self.partida.screen.tracer(0)
+                if self.partida.screen.delay() > 0:
+                    if self.partida.jugador1.ia:
+                        self.partida.screen.delay(self.partida.screen.delay() - 1)
+                    else:
+                        self.partida.screen.delay(self.partida.screen.delay() - .5)
+                angle = self.distance(self.partida.jugador1.xcor(), self.partida.jugador1.ycor()) + random.randint(1,
+                                                                                                                   25)
+                if self.ycor() <= self.partida.jugador1.ycor():
+                    angle = -angle
+                self.right(180 + angle)
+                self.partida.screen.tracer(1)
+                self.fd(15)
+                self.partida.jugador1.setx(self.partida.jugador1.xcor() + 3)
+                self.partida.jugador1.setx(self.partida.jugador1.xcor() + 2)
+                self.partida.jugador1.setx(self.partida.jugador1.xcor() + 1)
+        else:  # Choque con el jugador 2
+            if int(self.partida.jugador2.xcor()) - 20 <= int(self.xcor()) <= int(self.partida.jugador2.xcor()) \
+                    and (int(self.partida.jugador2.ycor()) - 50 <= int(self.ycor()) - 10 and int(
+                        self.ycor()) + 10 <= int(self.partida.jugador2.ycor()) + 50):
+                self.partida.jugador2.setx(self.partida.jugador2.xcor() + 1)
+                self.partida.jugador2.setx(self.partida.jugador2.xcor() + 2)
+                self.partida.jugador2.setx(self.partida.jugador2.xcor() + 3)
+                self.partida.screen.tracer(0)
+                if self.partida.screen.delay() > 0:
+                    if self.partida.jugador2.ia:
+                        self.partida.screen.delay(self.partida.screen.delay() - 1)
+                    else:
+                        self.partida.screen.delay(self.partida.screen.delay() - .5)
+                angle = self.distance(self.partida.jugador2.xcor(), self.partida.jugador2.ycor()) + random.randint(1,
+                                                                                                                   25)
+                if self.ycor() <= self.partida.jugador2.ycor():
+                    angle = -angle
+                self.right(180 + angle)
+                self.partida.screen.tracer(1)
+                self.fd(15)
+                self.partida.jugador2.setx(self.partida.jugador2.xcor() - 3)
+                self.partida.jugador2.setx(self.partida.jugador2.xcor() - 2)
+                self.partida.jugador2.setx(self.partida.jugador2.xcor() - 1)
 
 
-def bounce_car2():
-    """Comprobamos si la pelota toca el coche 2"""
-    if int(car2.xcor()) - 20 <= int(ball.xcor()) <= int(car2.xcor()) \
-            and (int(car2.ycor()) - 50 <= int(ball.ycor()) - 10 and int(ball.ycor()) + 10 <= int(car2.ycor()) + 50):
-        car2.setx(car2.xcor() + 5)
-        screen.tracer(0)
-        if screen.delay() > 0:
-            if enable_ia2:
-                screen.delay(screen.delay() - 1)
-            else:
-                screen.delay(screen.delay() - .5)
-        angle = ball.distance(car2.xcor(), car2.ycor()) + random.randint(1, 25)
-        if ball.ycor() <= car2.ycor():
-            angle = -angle
-        ball.right(180 + angle)
-        screen.tracer(1)
-        ball.fd(15)
-        car2.setx(car2.xcor() - 5)
+class Bate(Turtle):
+    def __init__(self, partida, fillcolor='white', izquierda=True, ia=False, velocidad=2):
+        Turtle.__init__(self)
+        self.up()
+        self.shape('square')
+        self.fillcolor(fillcolor)
+        self.pencolor('black')
+        self.shapesize(1, 5, 2)
+        self.left(90)
+        if izquierda:
+            self.setpos(-partida.screen.window_width() / 2 + 30, 0)
+        else:
+            self.setpos(partida.screen.window_width() / 2 - 30, 0)
+
+        self.partida = partida
+        self.ia = ia
+        self.velocidad = velocidad
+        self.moviendose = False
+        self.direccion = None
+
+    def mover(self, arriba=True):
+        self.moviendose = True
+        if arriba:
+            self.direccion = "arriba"
+        else:
+            self.direccion = "abajo"
+
+    def arriba(self):
+        self.mover(True)
+
+    def abajo(self):
+        self.mover(False)
+
+    def parar(self):
+        self.moviendose = False
+        self.direccion = None
+
+    def check_canvas(self):
+        partida.screen.tracer(0)
+        if self.direccion:
+            if self.direccion == "abajo":
+                return self.ycor() <= -self.partida.screen.window_height() / 2 + (20 * (self.shapesize()[1] - 1))
+            elif self.direccion == "arriba":
+                return self.ycor() >= self.partida.screen.window_height() / 2 - (20 * (self.shapesize()[1] - 1))
+        partida.screen.tracer(1)
 
 
-def move_turt_ball(turtobj, step=2, stop=multiprocessing.Event()):
-    """Movemos el objeto en pasos de 2"""
-    while not stop.is_set():
-        bounce_car1()
-        bounce_car2()
-        turtobj.forward(step)
-        check_canvas_ball(turtobj)
-        if enable_ia1:
-            move_car_ia(car1)
-        if enable_ia2:
-            move_car_ia(car2)
+class Marcador(Turtle):
+    def __init__(self, partida):
+        Turtle.__init__(self)
+        self.hideturtle()
+        self.up()
+        self.color('white')
+        self.partida = partida
+        self.position = (0, self.partida.screen.window_height() / 2 - 160)
+        self.setpos(self.position)
+        self.score = [0, 0]
+        self.refrescar()
+
+    def sumarpunto(self, jugador=0):
+        self.score[jugador] += 1
+
+    def refrescar(self):
+        self.clear()
+        self.write(str(self.score[0]) + "          " + str(self.score[1]), align="center",
+                   font=("Helvetica", 80, "normal"))
 
 
-def draw_field():
-    ball.clear()
-    ball.pensize(3)
-    ball.pencolor('white')
-    ball.speed(0)
-    ball.down()
-    ball.setpos(0, -screen.window_height() // 2 + 30)
-    ball.setpos(0, screen.window_height() // 2 - 30)
-    ball.fd(screen.window_width() // 2 - 30)
-    ball.right(90)
-    ball.fd(screen.window_height() // 2 - 30)
-    ball.fd(screen.window_height() // 2 - 30)
-    ball.right(90)
-    ball.fd(screen.window_width() // 2 - 30)
-    ball.fd(screen.window_width() // 2 - 30)
-    ball.right(90)
-    ball.fd(screen.window_height() // 2 - 30)
-    ball.fd(screen.window_height() // 2 - 30)
-    ball.right(90)
-    ball.fd(screen.window_width() // 2 - 30)
-    ball.up()
-    ball.home()
-    ball.speed(3)
+class Game:
+    def __init__(self):
+        self.screen = Screen()
+        self.screen.bgcolor('#0f0f0f')
+
+        self.jugando = True
+        self.activar_ia1, self.activar_ia2 = True, False
+        self.initial_delay = 3
+
+        self.marcador = Marcador(self)
+        self.pelota = Pelota(self)
+
+        self.pintar_campo()
+
+        self.jugador1 = Bate(self, 'red', True, self.activar_ia1)
+        self.jugador2 = Bate(self, 'blue', False, self.activar_ia2)
+
+    def pintar_campo(self):
+        self.pelota.clear()
+        self.pelota.pensize(3)
+        oldcolor = self.pelota.pencolor()
+        self.pelota.pencolor('white')
+        self.pelota.speed(0)
+        self.pelota.down()
+        self.pelota.setpos(0, -self.screen.window_height() // 2 + 30)
+        self.pelota.setpos(0, self.screen.window_height() // 2 - 30)
+        self.pelota.fd(self.screen.window_width() // 2 - 30)
+        self.pelota.right(90)
+        self.pelota.fd(self.screen.window_height() // 2 - 30)
+        self.pelota.fd(self.screen.window_height() // 2 - 30)
+        self.pelota.right(90)
+        self.pelota.fd(self.screen.window_width() // 2 - 30)
+        self.pelota.fd(self.screen.window_width() // 2 - 30)
+        self.pelota.right(90)
+        self.pelota.fd(self.screen.window_height() // 2 - 30)
+        self.pelota.fd(self.screen.window_height() // 2 - 30)
+        self.pelota.right(90)
+        self.pelota.fd(self.screen.window_width() // 2 - 30)
+        self.pelota.pencolor(oldcolor)
+        self.pelota.up()
+        self.pelota.home()
+        posneg = random.choice([-1, 1])
+        self.pelota.right(posneg * random.randint(155, 205))
+        self.pelota.speed(self.pelota.velocidad)
 
 
-def draw_marker():
-    marker.clear()
-    marker.write(str(score[0]) + "          " + str(score[1]), align="center", font=("Helvetica", 80, "normal"))
+if __name__ == "__main__":
+    partida = Game()
 
+    partida.screen.delay(partida.initial_delay)
 
-def move_car(car, fd, event):
-    while not event.is_set() and not check_canvas_car(car, fd < 0):
-        car.fd(fd)
+    if not partida.jugador1.ia:
+        partida.screen.onkeypress(partida.jugador1.arriba, "w")
+        partida.screen.onkeyrelease(partida.jugador1.parar, "w")
+        partida.screen.onkeypress(partida.jugador1.abajo, "s")
+        partida.screen.onkeyrelease(partida.jugador1.parar, "s")
+    if not partida.jugador2.ia:
+        partida.screen.onkeypress(partida.jugador2.arriba, "Up")
+        partida.screen.onkeyrelease(partida.jugador2.parar, "Up")
+        partida.screen.onkeypress(partida.jugador2.abajo, "Down")
+        partida.screen.onkeyrelease(partida.jugador2.parar, "Down")
+    partida.screen.listen()
 
+    while partida.jugando:
+        partida.screen.tracer(1)
+        partida.pelota.mover_pelota()
+        if partida.jugador1.moviendose:
+            if not partida.jugador1.check_canvas():
+                if partida.jugador1.direccion == "arriba":
+                    posneg = 1
+                else:
+                    posneg = -1
+                partida.jugador1.fd(posneg * partida.jugador1.velocidad)
+        if partida.jugador2.moviendose:
+            if not partida.jugador2.check_canvas():
+                if partida.jugador2.direccion == "arriba":
+                    posneg = 1
+                else:
+                    posneg = -1
+                partida.jugador2.fd(posneg * partida.jugador2.velocidad)
 
-def move_car_ia(car):
-    car.setpos(car.xcor(), ball.ycor())
-
-
-def move_car1_up():
-    stop_car1.clear()
-    proc = multiprocessing.Process(target=move_car, args=(car1, 2, stop_car1))
-    proc.start()
-
-
-def move_car1_down():
-    stop_car1.clear()
-    proc = multiprocessing.Process(target=move_car, args=(car1, -2, stop_car1))
-    proc.start()
-
-
-def move_car2_up():
-    stop_car2.clear()
-    proc = multiprocessing.Process(target=move_car, args=(car2, 2, stop_car2))
-    proc.start()
-
-
-def move_car2_down():
-    stop_car2.clear()
-    proc = multiprocessing.Process(target=move_car, args=(car2, -2, stop_car2))
-    proc.start()
-
-
-def stopev_car1():
-    stop_car1.set()
-
-
-def stopev_car2():
-    stop_car2.set()
-
-
-def main():
-    initialize_game()
-
-    if not enable_ia1:
-        screen.onkeypress(move_car1_up, "w")
-        screen.onkeyrelease(stopev_car1, "w")
-        screen.onkeypress(move_car1_down, "s")
-        screen.onkeyrelease(stopev_car1, "s")
-    if not enable_ia2:
-        screen.onkeypress(move_car2_up, "Up")
-        screen.onkeyrelease(stopev_car2, "Up")
-        screen.onkeypress(move_car2_down, "Down")
-        screen.onkeyrelease(stopev_car2, "Down")
-    screen.listen()
-
-
-main()
-
-turtle.done()
+    partida.screen.mainloop()
