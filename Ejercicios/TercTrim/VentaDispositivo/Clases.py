49f3f6b4314509659a00e089732bd66120139adb
Antonio Terrero <atalgaba@gmail.com>
2017-05-11 09:37:35 +0200

    repr cliente

--- Ejercicios/TercTrim/VentaDispositivo/Clases.py
+++ Ejercicios/TercTrim/VentaDispositivo/Clases.py
@@ -1,4 +1,3 @@
-
 class Cliente:
     def __init__(self, ncliente, nombre, telefono, direccion, ciudad):
         self.ncliente = ncliente
@@ -6,3 +5,7 @@ class Cliente:
         self.telefono = telefono
         self.direccion = direccion
         self.ciudad = ciudad
+
+    def __repr__(self):
+        return 'Cliente({}, {}, {}, {}, {})'.format(self.ncliente, self.nombre, self.telefono, self.direccion,
+                                                    self.ciudad)
