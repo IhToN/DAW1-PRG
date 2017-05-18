c368c4d3e0b494998f4a03d60bc48a905baeac36
Antonio Terrero <atalgaba@gmail.com>
2017-05-18 11:58:22 +0200

    repaso

--- Ejercicios/TercTrim/Ejercicio1.py
+++ Ejercicios/TercTrim/Ejercicio1.py
@@ -10,6 +10,23 @@ def numeros_pares():
         yield i
 
 
-a = numeros_pares()
-for e in a:
+# a = numeros_pares()
+# for e in a:
+#    print(e)
+
+
+"""
+    Generador de potencias sucesivas de un número.
+"""
+
+
+def potencia(n, t=5):
+    i = 0
+    while i < t:
+        i += 1
+        yield n ** i
+
+
+b = potencia(5, 10)
+for e in b:
     print(e)
